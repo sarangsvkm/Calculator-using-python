fc71c91d46686142647e76582a84e35272377769
sarangsvkm <sarangsvkm@gmail.com>
2022-03-01 11:38:25 +0530

    final project

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def clear_all():
     display.delete(0,END)
 
 def undo():
+    enter_string=display.get()
+    if len(enter_string):
+        new_string=enter_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+
+    else:
+        clear_all()
+        display.insert(0,"Error")
+
 
 
 
@@ -29,7 +39,7 @@ Button(root,text="8",command=lambda :get_variable(8)).grid(row=4,column=1)
 Button(root,text="9",command=lambda :get_variable(9)).grid(row=4,column=2)
 Button(root,text="0",command=lambda :get_variable(0)).grid(row=5,column=0)
 Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=1)
-Button(root,text="->").grid(row=5,column=2)
+Button(root,text="->",command=lambda :undo()).grid(row=5,column=2)
 Button(root,text="=").grid(row=2,column=3)
 Button(root,text="+").grid(row=3,column=3)
 Button(root,text="-").grid(row=4,column=3)
