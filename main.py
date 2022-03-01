6385f89b1d40686cad9338c871f95bbc1ee0dfa4
sarangsvkm <sarangsvkm@gmail.com>
2022-03-01 10:24:01 +0530

    final project

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def get_variable(num):
 def clear_all():
     display.delete(0,END)
 
+def undo():
+
+
 
 
 Button(root,text="1",command=lambda :get_variable(1)).grid(row=2,column=0)
@@ -25,9 +28,9 @@ Button(root,text="7",command=lambda :get_variable(7)).grid(row=4,column=0)
 Button(root,text="8",command=lambda :get_variable(8)).grid(row=4,column=1)
 Button(root,text="9",command=lambda :get_variable(9)).grid(row=4,column=2)
 Button(root,text="0",command=lambda :get_variable(0)).grid(row=5,column=0)
-Button(root,text="AC").grid(row=5,column=1)
-Button(root,text="=").grid(row=5,column=2)
-Button(root,text=".").grid(row=2,column=3)
+Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=1)
+Button(root,text="->").grid(row=5,column=2)
+Button(root,text="=").grid(row=2,column=3)
 Button(root,text="+").grid(row=3,column=3)
 Button(root,text="-").grid(row=4,column=3)
 Button(root,text="*").grid(row=5,column=3)
